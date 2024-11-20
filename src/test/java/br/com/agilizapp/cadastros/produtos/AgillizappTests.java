2d33355052bd37cbe33ec339713e20fca1c09c17
Antonio Ishac <antonioishac@gmail.com>
2024-11-20 20:57:36 -0300

    Disponibilizando o projeto e as tabelas de cadastro de produtos

---
+++ src/test/java/br/com/agilizapp/cadastros/produtos/AgillizappTests.java
@@ -0,0 +1,13 @@
+//package br.com.agilizapp.cadastros.produtos;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class AgillizappTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
